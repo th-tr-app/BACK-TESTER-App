b8826f2bd19f4d670ad82e21d71dec57c3fe7a77
th-tr-app <t.hrs001@gmail.com>
2025-11-27 14:58:44 +0900

    Adjust header styles in app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ st.logo("image_11.png", icon_image="image_10.png")
 # タイトルを2行に分ける（メイン＋小見出し）
 st.markdown("""
     <div style='margin-bottom: 20px;'>
-        <h1 style='font-weight: 450; font-size: 45px; margin: 0; padding: 0;'>BACK TESTER</h1>
-        <h3 style='font-weight: 250; font-size: 20px; margin: 0; padding: 0; color: #aaaaaa;'>DAY TRADING MANAGER｜ver 1.2</h3>
+        <h1 style='font-weight: 400; font-size: 45px; margin: 0; padding: 0;'>BACK TESTER</h1>
+        <h3 style='font-weight: 300; font-size: 18px; margin: 0; padding: 0; color: #aaaaaa;'>DAY TRADING MANAGER｜ver 1.2</h3>
     </div>
     """, unsafe_allow_html=True)
 
