4bcfde28f2a9aee9006a6224091ac0983afa0520
th-tr-app <t.hrs001@gmail.com>
2026-01-12 01:06:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -77,14 +77,14 @@ st.markdown("""
 /* 見出し（###）のサイズを一括で小さくする */
     h3 {
         font-size: 1.4rem !important;
-        font-weight: 500 !important;
+        font-weight: 600 !important;
         margin-top: 10px !important;
     }
     
 /* 見出し2（####）のサイズを一括で小さくする */
     h4 {
         font-size: 1.4rem !important;
-        font-weight: 500 !important;
+        font-weight: 600 !important;
         margin-top: 10px !important;
     }
     
